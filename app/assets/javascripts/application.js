052c3a42a961c0aecd85a8739dd7d74bd946bc40
Ajeng Ardhia Arya Kusuma Putri <ardhia23@gmail.com>
2018-10-17 08:10:32 +0700

    Add files via upload

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,30 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+//
+
+//= require jquery/jquery-2.1.1.js
+//= require bootstrap-sprockets
+//= require metisMenu/jquery.metisMenu.js
+//= require pace/pace.min.js
+//= require peity/jquery.peity.min.js
+//= require slimscroll/jquery.slimscroll.min.js
+//= require inspinia.js
+//= require jquery_ujs
+//= require bootstrap-datepicker
+//= require select2
+//= require toastr/toastr.min.js
+
+//= require iCheck/icheck.min.js
+//= require jeditable/jquery.jeditable.js
+//= require dataTables/jquery.dataTables.js
+//= require dataTables/dataTables.bootstrap.js
+//= require dataTables/dataTables.responsive.js
+//= require dataTables/dataTables.tableTools.min.js
+//= require jqGrid/i18n/grid.locale-el.js
+//= require jqGrid/jquery.jqGrid.min.js
+//= require jquery-ui/jquery-ui.min.js
+//= require datapicker/bootstrap-datepicker.js
